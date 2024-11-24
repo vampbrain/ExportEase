f0d8c1e09b9df08a761bab1168b6ab412b08fa0e
aswin-codes <aswinraaj.ps@gmail.com>
2024-11-24 13:30:56 +0530

    removed track and signup

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from './features/login/LoginPage';
-import SignupPage from './features/signup/SignupPage';
-import TrackPage from './features/track/TrackPage';
 import HomePage from './features/home/HomePage';
 import CompliancePage from './features/compliance/CompliancePage';
 import ShippingEstimator from './features/priceEstimation/PriceEstimation';
@@ -29,8 +27,6 @@ const App = () => {
           <Routes>
             <Route path='/' element={<HomePage/>} />
             <Route path="/login" element={<LoginPage/>} />
-            <Route path='/signup' element={<SignupPage/>} />
-            <Route path='/track' element={<TrackPage/>} />
             <Route path='/estimate' element={<ShippingEstimator/>}/>
             <Route path='/compliance' element={<CompliancePage/>}/>
           </Routes>
